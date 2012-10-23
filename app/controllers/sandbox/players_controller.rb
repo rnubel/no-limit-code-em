030fb438b51eae651940350f9adbbfe95bbe6c69
Shaan Sapra <ssapra@uchicago.edu>
2012-10-23 18:07:57 -0500

    sandbox working with get and post requests for static information

--- app/controllers/sandbox/players_controller.rb
+++ app/controllers/sandbox/players_controller.rb
@@ -20,12 +20,13 @@ module Sandbox
       case params[:id]
       when "fc2455f8-1879-4ad4-8012-1f337c2869f2"
         json = '{"name":"Bill9","initial_stack":250,"your_turn":true,"current_bet":20,"minimum_bet":25,"maximum_bet":250,"hand":["6D","5C","AS","5H","9H"],"betting_phase":"deal","players_at_table":[{"player_name":"Bill8","initial_stack":250,"current_bet":25,"actions":[{"action":"ante","amount":20},{"action":"bet","amount":25}]},{"player_name":"Bill9","initial_stack":250,"current_bet":20,"actions":[{"action":"ante","amount":20}]}],"table_id":88,"round_id":105,"round_history":[{"round_id":105,"table_id":88,"stack_change":null}],"lost_at":null}'  
-        return render_bad_request "Amount must be a positive integer" if 
-          params[:amount] && (params[:amount].to_i < 0)
+        return render_bad_request "Amount must be a positive integer" if params[:amount] && (params[:amount].to_i < 0)
       when "728f53dd-a5dc-4582-8864-be37576b9592"
         json = '{"name":"Bill8","initial_stack":250,"your_turn":true,"current_bet":25,"minimum_bet":25,"maximum_bet":250,"hand":["2C","7H","8C","3D","2D"],"betting_phase":"draw","players_at_table":[{"player_name":"Bill8","initial_stack":250,"current_bet":25,"actions":[{"action":"ante","amount":20},{"action":"bet","amount":25}]},{"player_name":"Bill9","initial_stack":250,"current_bet":25,"actions":[{"action":"ante","amount":20},{"action":"bet","amount":25}]}],"table_id":92,"round_id":110,"round_history":[{"round_id":110,"table_id":92,"stack_change":null}],"lost_at":null}'
-        return render_bad_request 'Action must be set to "replace"' if
-          params[:action_name] != "replace"
+        return render_bad_request "action_name must be set to replace instead of #{params[:action_name]}" if params[:action_name] != "replace"
+        return render_bad_request 'cards must be an string of ' if !(cards_value(params[:cards]).is_a? Enumerable)
+        logger.debug "Cards: #{params[:cards]}"
+        logger.debug "Cards?: #{cards_value(params[:cards])}"
       else
         return render_not_found "Invalid key. Should look like #{SecureRandom.uuid}"
       end 
@@ -33,11 +34,11 @@ module Sandbox
       action = { :action => params[:action_name], 
                  :amount => params[:amount] && params[:amount].to_i, 
                  :cards =>  cards_value(params[:cards]) }
-      if valid_action?(JSON.parse(json), action_params)
-        json = '{:action => valid}'
+      if valid_action?(JSON.parse(json), action)
+        json = '{"action":"valid"}'
         render :json => JSON.parse(json)
       else
-        json = '{:action => invalid}'
+        json = '{"action":"invalid"}'
         render :json => JSON.parse(json)
       end
     end
@@ -48,14 +49,14 @@ module Sandbox
       case action[:action]
           when "bet" # Absolute amount!            
             amount = action[:amount].to_i
-            raised_amount = amount - (json[:current_bet] || 0)
+            raised_amount = amount - (json["current_bet"] || 0)
 
-            (amount >= json[:minimum_bet] || raised_amount == json[:stack]) && raised_amount <= json[:stack]
+            (amount >= json["minimum_bet"] || raised_amount == json["initial_stack"]) && raised_amount >= 0
           when "replace" # List of cards!
             return false unless action[:cards].is_a? Enumerable
             action[:cards].all? { |c|
-              json[:hand].include?(c)
-            } && action[:cards].size <= DRAW_LIMIT
+              json["hand"].include?(c)
+            } && action[:cards].size <= 3
           when "fold" # Should always be allowed, if it's their turn.
             true
           end
