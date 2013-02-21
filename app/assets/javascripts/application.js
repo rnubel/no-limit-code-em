33f496ff1de58acd5b1c1781956452acb5365b38
brousalis <brousapg@gmail.com>
2013-02-21 12:15:48 -0600

    Starting the views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,31 +1,30 @@
 //= require jquery
 //= require bootstrap-tab
 //= require_self
-
 $(function() {
-  reload_page();
-
+  reload_tables();
+  reload_scoreboard();
   var refresh = getURLParameter("refresh");
+  var delay = (getURLParameter("delay") == "pete" ? 1000 : 10000);
   if(refresh == "true") {
-    setInterval(function() {
-      reload_page();
-    }, 1000);
+    setInterval(function() { reload_tables(); }, delay);
+    setInterval(function() { reload_scoreboard(); }, 5000);
   }
 })
-
-function reload_page() {
-  $('.loader').fadeIn();
+function reload_tables() {
   $.ajax({
     url: '/tournaments/refresh',
-    type: 'GET',
-    success: function() {
-      $('.loader').fadeOut();
+    type: 'POST',
+    success: function(e) {
+      console.log(e);
     }
   });
 }
+function reload_scoreboard() {
 
+}
 function getURLParameter(name) {
-    return decodeURI(
-        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
-    );
+  return decodeURI(
+    (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+  );
 }
