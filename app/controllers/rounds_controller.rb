2bcfc9c311f32d7b81b3b389614bb6c240274ae6
Robert Nubel <rnubel@enovafinancial.com>
2013-10-15 11:15:16 -0500

    Fixes & reg updates

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,5 +1,4 @@
 class RoundsController < ApplicationController
-
   def show
     @round = Round.find(params[:id])
     @players = @round.tournament.players.select([:id, :name, :initial_stack]).reduce({}) { |h, p| h[p.id] = p;h } 
