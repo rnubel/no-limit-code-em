98628c809f8a3da5706d0d890dbd309d7dba193e
Pete Brousalis <brousapg@gmail.com>
2013-10-17 15:49:56 -0500

    Tons of changes

--- lib/tournament_presenter.rb
+++ lib/tournament_presenter.rb
@@ -30,7 +30,7 @@ class TournamentPresenter
                                                         :initial_stack => p.current_player_state(:initial_stack),
                                                         :stack => stack_display(p.current_player_state(:stack),false),
                                                         :hand => build_hand(p.current_player_state(:hand)),
-                                                        :current_bet => p.current_player_state(:current_bet) }},
+                                                        :current_bet => p.current_player_state(:current_bet) || 0 }},
         :latest_winners => table.rounds
                                 .ordered
                                 .where(:playing => false)
