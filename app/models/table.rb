9e5d063321f631a96da225adeaab51e4a169e8c6
umtrey <dfsiii@gmail.com>
2013-10-24 23:12:22 -0400

    well this is something

--- app/models/table.rb
+++ app/models/table.rb
@@ -90,8 +90,12 @@ class Table < ActiveRecord::Base
 
   def dealer_for_new_round
     if r = current_round
-      active_players.find { |p| p.id > r.dealer_id } ||
-       active_players.first
+      player_list = active_players.collect(&:id)
+      if active_players.select { |p| p.id > r.dealer_id }.empty?
+        return Player.find_by_id(player_list.min)
+      else
+        return Player.find_by_id(active_players.select { |p| p.id > r.dealer_id }.min)
+      end
     else
       active_players.first
     end
